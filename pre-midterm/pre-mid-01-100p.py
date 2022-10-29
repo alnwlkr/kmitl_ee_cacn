9889ae1db06997f0963543c785a025bba713f635
alnwlkr <fostgeo5@gmail.com>
2022-10-29 20:38:56 +0700

    change directory and remove trash code

--- pre-midterm/pre-mid-01-100p.py
+++ pre-midterm/pre-mid-01-100p.py
@@ -8,12 +8,14 @@ else:
     price = 487.26 + 1055.45 + 4.4217*(n-400)
 
 #Service Charge
-if n != 0:
+if n > 0:
 	price += 38.22
-#FT
+
 price += n*(0.9343)
-#VAT
 price = price*(1.07)
+    
+#FT
+#VAT
 if n <= 0:
     price = 0
 print('%.2f' %(price))
